e8d90e27d5e9653d0db20bb2ebd80562cee0b89a
yuri <yuri.fedorov@avalon.au>
2025-02-20 18:21:03 +0300

    updates

--- src/resultsParser.ts
+++ src/resultsParser.ts
@@ -1,18 +1,14 @@
-const saveResults = (runId: string, browser: string, platform: string, testData: any): void => {
+const saveResults = (
+    runId: string,
+    browser: string,
+    platform: string,
+    testData: any
+): void => {
 
-    console.log("🚀 Получены данные:");
     console.log(`runId: ${runId}`);
     console.log(`browser: ${browser}`);
     console.log(`platform: ${platform}`);
-
-    const cleanedTestData = JSON.parse(JSON.stringify(testData, (key, value) => {
-        if (key === "embeddings") {
-            return;
-        }
-        return value;
-    }));
-
-    console.log("testData':", JSON.stringify(cleanedTestData, null, 2));
+    console.log("testData:", JSON.stringify(testData, null, 2));
 
     console.log("✅ Функция saveResults выполнена успешно!");
 };

58d3fb149baa81ae01d0ed9934b60fbdca8f55e2
yuri <yuri.fedorov@avalon.au>
2025-02-20 18:00:42 +0300

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
-import { runTests } from "./testRunner";
 import { saveResults } from "./resultsParser";
 import { sendSlackReport } from "./slackNotifier";
 
 const runner = {
-    runTests,
     saveResults,
     sendSlackReport
 };

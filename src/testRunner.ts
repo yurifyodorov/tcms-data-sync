f7f8a78e4f34f18916cf37227811ec42fe79861e
yuri <yuri.fedorov@avalon.au>
2025-02-20 17:09:28 +0300

    remove cli

--- src/testRunner.ts
+++ src/testRunner.ts
@@ -1,5 +1,16 @@
-const runTests = (): void => {
+const runTests = (
+    runId: string,
+    specString: string[],
+    browser: string,
+    platform: string,
+): void => {
     console.log("✅ Функция runTests выполнена успешно!");
+    console.log(`
+        runId: ${runId}, 
+        specString: ${specString.join(', ')}, 
+        browser: ${browser}, 
+        platform: ${platform}
+    `);
 };
 
 export { runTests };

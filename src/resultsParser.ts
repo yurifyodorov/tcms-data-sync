4398667244c2cd86ddab68e1c193bb68e657d047
yuri <yuri.fedorov@avalon.au>
2025-02-20 18:15:06 +0300

    updates

--- src/resultsParser.ts
+++ src/resultsParser.ts
@@ -1,4 +1,15 @@
-const saveResults = (): void => {
+const saveResults = (
+    runId: string,
+    browser: string,
+    platform: string,
+    testData: any
+): void => {
+
+    console.log(`runId: ${runId}`);
+    console.log(`browser: ${browser}`);
+    console.log(`platform: ${platform}`);
+    console.log("testData:", JSON.stringify(testData, null, 2));
+
     console.log("✅ Функция saveResults выполнена успешно!");
 };
 

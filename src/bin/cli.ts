3d94955e676c606bbf64317ad8808c88e2e5dcad
yuri <yuri.fedorov@avalon.au>
2025-02-20 09:53:09 +0300

    updates

--- src/bin/cli.ts
+++ src/bin/cli.ts
@@ -1,14 +1,30 @@
 #!/usr/bin/env node
 
-import runTests from "../testRunner";
+import { runTests } from "../testRunner";
 import saveBrowserDetails from "../browserInfo";
 import saveSystemInfo from "../systemInfo";
 import saveResults from "../resultsParser";
 import sendSlackReport from "../slackNotifier";
+import * as process from "process";
+
+const args = process.argv.slice(2);
+
+if (args.length < 2) {
+    console.error("Ошибка: Необходимо указать пути к спецификациям и браузер.");
+    process.exit(1);
+}
+
+const specPaths = args[0].split(',');
+const browser = args[1];
 
 console.log("🚀 Запуск tcms-test-runner...");
+console.log(`Используем браузер: ${browser}`);
+console.log(`Пути спецификаций: ${specPaths}`);
 
-runTests();
+// Запускаем тесты с переданными аргументами
+runTests(specPaths, browser);
+
+// Дополнительные операции
 saveBrowserDetails();
 saveSystemInfo();
 saveResults();

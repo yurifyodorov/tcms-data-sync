4bdbd5a0eed63d132a5442b594c07398ad4a4b4a
yuri <yuri.fedorov@avalon.au>
2025-02-20 13:06:04 +0300

    updates

--- src/testRunner.ts
+++ src/testRunner.ts
@@ -1,19 +1,14 @@
 import { execSync } from 'child_process';
-import { createId } from './lib/id';
 
-const runTests = (specPaths: string[], browser: string): void => {
-    const runId = createId();
+const runTests = (specPaths: string[], browser: string, runId: string): void => {
     console.log(`🟢 Running tests with runId: ${runId}`);
-
     const specString = specPaths.join(',');
     console.log('Test spec paths:', specString);
 
     try {
         const command = `cypress run --browser ${browser} --spec "${specString}" --env browserName=${browser},runId=${runId} --no-runner-ui --headless`;
-
         console.log(`Running Cypress command: ${command}`);
         execSync(command, { stdio: 'inherit' });
-
         console.log(`✅ Tests executed successfully`);
     } catch (error: any) {
         console.error(`❌ Error during test execution:`, error.message);

f35a50af8cd0b73beddc40d56adadb76b1ca4bd3
yurifyodorov <yuri.inpost@gmail.com>
2025-07-01 14:09:36 +0300

    updates

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,15 +1,28 @@
 import { PrismaClient } from "../../prisma-client";
 
+declare global {
+    // eslint-disable-next-line no-var
+    var __dbClient: PrismaClient | undefined;
+}
+
 export const getDbClient = (databaseUrl: string): PrismaClient => {
     if (!databaseUrl) {
         throw new Error("databaseUrl is not provided");
     }
 
-    return new PrismaClient({
+    if (global.__dbClient) {
+        return global.__dbClient;
+    }
+
+    const client = new PrismaClient({
         datasources: {
             db: {
                 url: databaseUrl,
             },
         },
     });
+
+    global.__dbClient = client;
+
+    return client;
 };
